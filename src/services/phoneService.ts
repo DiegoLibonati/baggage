7a5a1eef8bb7b745beb882a051aa861c27c01378
Die <dlibonati@leafnoise.io>
2026-05-01 19:57:00 -0300

    feat: better response type

--- src/services/phoneService.ts
+++ src/services/phoneService.ts
@@ -1,16 +1,15 @@
 import type { Phone } from "@/types/app";
+import type { ResponseDirect } from "@/types/responses";
 
 const phoneService = {
-  getAll: async (): Promise<Phone[]> => {
+  getAll: async (): Promise<ResponseDirect<Phone[]>> => {
     const response = await fetch("/react-useReducer-cart-project", {
       method: "GET",
     });
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    const phones: Phone[] = (await response.json()) as Phone[];
-
-    return phones;
+    return (await response.json()) as ResponseDirect<Phone[]>;
   },
 };
 
